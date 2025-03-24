6aef7527810a3c78a491a295adb28bb9efde1d07
Florent Uzio <fuzio@ripple.com>
2025-03-24 18:00:15 +0400

    removed keys

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
 import { v4 as uuidv4 } from "uuid"
 import { AuthService } from "./services/auth.service"
 import { CryptoService } from "./services/keypair.service"
-// const privateKeyPem = `-----BEGIN EC PRIVATE KEY-----
-// MHQCAQEEINFsnpHF+1B6yBZ/O3AbcdNkHrEdgqLiKtk7Sa5xmCMKoAcGBSuBBAAK
-// oUQDQgAE1vVFPhh0oYSg13YDMBXDdW4fWXwckuKiGI6J8bZKUyGLisARc1xiMYBu
-// gs4ZryKI4Wxzu1NIbSqbUysp+H+gAw==
-// -----END EC PRIVATE KEY-----`
 
-// const privateKey2 = `-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEINFsnpHF+1B6yBZ/O3AbcdNkHrEdgqLiKtk7Sa5xmCMKoAcGBSuBBAAKoUQDQgAE1vVFPhh0oYSg13YDMBXDdW4fWXwckuKiGI6J8bZKUyGLisARc1xiMYBugs4ZryKI4Wxzu1NIbSqbUysp+H+gAw==\n-----END EC PRIVATE KEY-----`
-
-// const publicKey =
-//   "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE1vVFPhh0oYSg13YDMBXDdW4fWXwckuKiGI6J8bZKUyGLisARc1xiMYBugs4ZryKI4Wxzu1NIbSqbUysp+H+gAw=="
-
-const publicKey =
-  "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEOYGYFXBKH/kp/0IYEPmi/UWlnoBzqPkVJQ+MkITioyyiLLBHnjDCkRzJyPPlW/uQc4WzaW/1P9GBo8oHh4P+qw=="
-
-const privateKKey = `-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIPiVH02UDoRm2ENdox12UHyTB7EDY3jEGJKECYLGWwvwoAoGCCqGSM49AwEHoUQDQgAEOYGYFXBKH/kp/0IYEPmi/UWlnoBzqPkVJQ+MkITioyyiLLBHnjDCkRzJyPPlW/uQc4WzaW/1P9GBo8oHh4P+qw==\n-----END EC PRIVATE KEY-----`
+const privateKey = process.env.PRIVATE_KEY ?? ""
+const publicKey = process.env.PUBLIC_KEY ?? ""
 
 const main = async () => {
   try {
     const uuid = uuidv4()
     const keypairService = new CryptoService()
-    const { signature } = keypairService.signMessage(privateKKey, uuid)
+    const { signature } = keypairService.signMessage(privateKey, uuid)
     console.log(signature)
 
     // Initialize services
