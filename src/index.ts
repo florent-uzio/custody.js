268f5bbc79435468e4fca84d7e280d50573f3034
Florent Uzio <fuzio@ripple.com>
2025-08-21 23:25:46 +0200

    export intent types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 export * from "./ripple-custody.js"
 export * from "./ripple-custody.types.js"
+
+// intents types
+export {
+  type Core_ApproveIntentBody,
+  type Core_GetIntentPathParams,
+  type Core_GetIntentsPathParams,
+  type Core_GetIntentsQueryParams,
+  type Core_IntentDryRunRequest,
+  type Core_IntentDryRunResponse,
+  type Core_IntentResponse,
+  type Core_ProposeIntentBody,
+  type Core_RejectIntentBody,
+  type Core_RemainingDomainUsers,
+  type Core_RemainingUsersIntentPathParams,
+  type Core_RemainingUsersIntentQueryParams,
+} from "./services/intents/index.js"
