1eead43a53ebb29153b0bc70333697f9ab87c99c
Florent Uzio <fuzio@ripple.com>
2026-01-19 00:10:51 +0400

    fix format

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { RippleCustody } from "./ripple-custody.js"
 export { DEFAULT_TIMEOUT_MS } from "./constants/index.js"
+export { RippleCustody } from "./ripple-custody.js"
 export type { RippleCustodyClientOptions } from "./ripple-custody.types.js"
 
 // keypairs types
